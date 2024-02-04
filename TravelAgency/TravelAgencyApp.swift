6d02dcb919ffddd5595ba198d189ef31fda84f84
joaobug <joaoenfrance@gmail.com>
2024-02-04 19:55:51 -0300

    Initial Commit

---
+++ TravelAgency/TravelAgencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAgencyApp.swift
+//  TravelAgency
+//
+//  Created by Joao Leal on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelAgencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
